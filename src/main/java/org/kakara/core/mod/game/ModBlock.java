e6ee88ecdbca8fa83795f16d725d93de850a703e
wherkamp <wherkamp@kingtux.me>
2019-12-30 16:50:19 -0500

    Added Hardness Annotation.

--- src/main/java/org/kakara/core/mod/game/ModBlock.java
+++ src/main/java/org/kakara/core/mod/game/ModBlock.java
@@ -1,6 +1,13 @@
 package org.kakara.core.mod.game;
 
+import org.kakara.core.annotations.Hardness;
+import org.kakara.core.annotations.Weight;
 import org.kakara.core.game.Block;
 
 public abstract class ModBlock extends ModItem implements Block{
+    @Override
+    public float getHardness() {
+        Hardness hardness = getClass().getAnnotation(Hardness.class);
+        return hardness == null ? 0.0f : hardness.value();
+    }
 }
