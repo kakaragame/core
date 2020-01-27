25fe15ab00f542c71ef3ed5ca9b149c9339c9009
wherkamp <wherkamp@kingtux.me>
2020-01-27 16:16:46 -0500

    Kinda did a resource class.

--- src/main/java/org/kakara/core/resources/Resource.java
+++ src/main/java/org/kakara/core/resources/Resource.java
@@ -1,4 +1,25 @@
 package org.kakara.core.resources;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Resource {
+    private File file;
+
+    public Resource(File file) {
+        this.file = file;
+    }
+
+    public URL getURL() {
+        try {
+            return file.toURI().toURL();
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
 }
