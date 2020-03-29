e8d0d49c0bf29baf4a21e28bc278dd70ab1e1ea1
wherkamp <wherkamp@kingtux.me>
2020-03-29 18:15:57 -0400

    Added getAllTextures

--- src/main/java/org/kakara/core/resources/ResourceManager.java
+++ src/main/java/org/kakara/core/resources/ResourceManager.java
@@ -3,12 +3,15 @@ package org.kakara.core.resources;
 import org.kakara.core.GameInstance;
 import org.kakara.core.mod.Mod;
 
+import java.util.List;
+
 //~/.kakara/resources/mods/{mod_name}/{resource_type}/path
 //~/.kakara/resources/mods/{mod_name}/texture/{resolution}/path
 //jar paths.
 // resources/{resource_type}/path
 // resources/texture/{resolution}/path
 public interface ResourceManager {
+    List<Resource> getAllTextures(TextureResolution resolution);
 
     void registerResource(String path, ResourceType resourceType, Mod mod);
 
