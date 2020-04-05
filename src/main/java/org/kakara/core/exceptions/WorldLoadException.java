c84728d3ab83def8c252e06e3e9396e1bb90aa61
wherkamp <wherkamp@kingtux.me>
2020-04-04 21:48:23 -0400

    Improved WorldLoadException

--- src/main/java/org/kakara/core/exceptions/WorldLoadException.java
+++ src/main/java/org/kakara/core/exceptions/WorldLoadException.java
@@ -1,7 +1,18 @@
 package org.kakara.core.exceptions;
 
 public class WorldLoadException extends Exception {
-    public WorldLoadException(String worldName) {
-        super("Unable to load " + worldName);
+    public WorldLoadException(String message) {
+        super(message);
+    }
+
+    public WorldLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WorldLoadException(Throwable cause) {
+        super(cause);
+    }
+
+    public WorldLoadException() {
     }
 }
