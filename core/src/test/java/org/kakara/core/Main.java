14d8b448d8c0f41950f70de53e8a802ce96ce9a0
wherkamp <wherkamp@kingtux.me>
2020-05-12 19:53:20 -0400

    Compiler Fixes

--- core/src/test/java/org/kakara/core/Main.java
+++ core/src/test/java/org/kakara/core/Main.java
@@ -8,12 +8,30 @@ import org.kakara.core.modinstance.ModInstanceType;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 public class Main {
     private static Gson gson = new Gson();
 
     public static void main(String[] args) {
+        NameKey nameKey = new NameKey("bob", "key");
+        NameKey nameKeyTwo = new NameKey("bob", "kesy");
+        System.out.println(nameKey.hashCode());
+        System.out.println(nameKeyTwo.hashCode());
+        Map<Integer, String> map = new HashMap<>();
+        long start = System.currentTimeMillis();
+        System.out.println(start);
+        for (int i = 0; i < 12000000; i++) {
+            map.put(i, String.valueOf(i));
+            NameKey one = new NameKey("bob", "key");
+            one.hashCode();
+            nameKey.equals(nameKeyTwo);
+        }
+        System.out.println(start - System.currentTimeMillis());
 
     }
 }
