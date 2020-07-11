9a1986b58a84d829b6fdfe4a65936ea5ebd58b2a
Wyatt J Herkamp <wherkamp@kingtux.me>
2020-07-11 15:49:31 -0400

    Texture Improvement

--- core/src/main/java/org/kakara/core/resources/Texture.java
+++ core/src/main/java/org/kakara/core/resources/Texture.java
@@ -5,6 +5,7 @@ import org.kakara.core.mod.Mod;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Texture {
     private Map<TextureResolution, Resource> resolutionResourceMap = new HashMap<>();
@@ -67,6 +68,20 @@ public class Texture {
 
     public Mod getMod() {
         return mod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Texture texture = (Texture) o;
+        return Objects.equals(getPath(), texture.getPath()) &&
+                Objects.equals(getMod(), texture.getMod());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPath(), getMod());
     }
 
     public TextureResolution getDefaultResolution() {
