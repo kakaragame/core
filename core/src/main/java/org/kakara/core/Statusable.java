19420cc3e20c843c79b19016f828eefe3d3b82e4
Wyatt J Herkamp <wherkamp@kingtux.me>
2020-07-16 22:22:20 -0400

    Well, it's doing something.

---
+++ core/src/main/java/org/kakara/core/Statusable.java
@@ -0,0 +1,6 @@
+package org.kakara.core;
+
+
+public interface Statusable {
+    Status getStatus();
+}
--- core/src/main/java/org/kakara/core/world/ChunkContent.java
+++ core/src/main/java/org/kakara/core/world/ChunkContent.java
@@ -1,6 +1,7 @@
 package org.kakara.core.world;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ChunkContent {
     private final List<GameBlock> gameBlocks;
@@ -26,5 +27,18 @@ public class ChunkContent {
 
     public boolean isNullChunk() {
         return gameBlocks == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChunkContent that = (ChunkContent) o;
+        return Objects.equals(getLocation(), that.getLocation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLocation());
     }
 }
