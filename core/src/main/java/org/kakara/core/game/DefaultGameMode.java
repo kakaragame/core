116a3b201aaba904a04a1e8753def1300fc6b2df
Wyatt J Herkamp <wherkamp@kingtux.me>
2020-07-30 11:42:19 -0400

    GameModeProperties

--- core/src/main/java/org/kakara/core/game/DefaultGameMode.java
+++ core/src/main/java/org/kakara/core/game/DefaultGameMode.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import static org.kakara.core.game.GameModeProperties.*;
 
 public enum DefaultGameMode implements GameMode {
-    CREATIVE(Set.of(CREATIVE_INVENTORY, INSTANT_BREAKING)),
+    CREATIVE(Set.of(CREATIVE_INVENTORY, INSTANT_BREAKING, UNBREAKABLE,CAN_FLY)),
     SURVIVAL(Set.of(SURVIVAL_INVENTORY));
     private final Set<String> properties;
 
