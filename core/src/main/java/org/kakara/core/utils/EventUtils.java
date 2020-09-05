98ded91320cbfb70db13808178bc026d6d57f16b
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-09-05 09:03:53 -0400

    EventUtils

---
+++ core/src/main/java/org/kakara/core/utils/EventUtils.java
@@ -0,0 +1,17 @@
+package org.kakara.core.utils;
+
+import org.kakara.core.events.Event;
+import org.kakara.core.events.FunctionalEvent;
+
+public class EventUtils {
+    /**
+     * Executes a Functional Event if not null
+     *
+     * @param functionalEvent the FunctionalEvent
+     * @param t               the Event Object
+     * @param <T>             the Event Type
+     */
+    public <T extends Event> void executeIfNotNull(FunctionalEvent<T> functionalEvent, T t) {
+        if (functionalEvent != null) functionalEvent.execute(t);
+    }
+}
