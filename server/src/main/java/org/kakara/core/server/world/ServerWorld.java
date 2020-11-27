03521bb30bd11d71031a5c6bfac70a3a4548cfd6
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-11-27 17:42:24 -0500

    Starting the World code

--- server/src/main/java/org/kakara/core/server/world/ServerWorld.java
+++ server/src/main/java/org/kakara/core/server/world/ServerWorld.java
@@ -1,6 +1,8 @@
 package org.kakara.core.server.world;
 
 import org.jetbrains.annotations.NotNull;
+import org.kakara.core.common.Serverable;
+import org.kakara.core.common.exceptions.NoServerVersionAvailableException;
 import org.kakara.core.common.game.ItemStack;
 import org.kakara.core.common.world.GameBlock;
 import org.kakara.core.common.world.Location;
@@ -9,6 +11,21 @@ import org.kakara.core.common.world.World;
 import java.util.Optional;
 
 public interface ServerWorld extends World {
+    @Override
+    default boolean isServerVersionAvailable() {
+        return true;
+    }
+
+    @Override
+    default <T extends Serverable> T getServerVersion() {
+        return (T) this;
+    }
+
+    @Override
+    default void requiresServerVersion() throws NoServerVersionAvailableException {
+
+    }
+
     @NotNull
     Optional<GameBlock> setBlock(@NotNull ItemStack itemStack, @NotNull Location location);
 
