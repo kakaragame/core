164191f04991ddafaf69acd08cdc1f2ad12eab26
7we <peterhenryd@gmail.com>
2020-11-28 07:15:10 -0500

    Dependency fixes.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation("com.google.code.gson", "gson", "2.8.6")
     implementation("org.slf4j", "slf4j-api", "1.7.30")
+    implementation("org.jetbrains", "annotations", "16.0.1")
 }
