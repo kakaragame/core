0e905e1f5b8bb76e12defa6b9dda28af9942522c
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-11-29 14:51:22 -0500

    Added code base for Dynamic Engine Rendering

--- common/src/main/java/org/kakara/core/common/engine/EngineCore.java
+++ common/src/main/java/org/kakara/core/common/engine/EngineCore.java
@@ -2,12 +2,14 @@ package org.kakara.core.common.engine;
 
 
 import org.kakara.core.common.gui.EngineController;
+import org.kakara.core.common.gui.dynamic.DynamicEngineInventoryRenderer;
 
 /**
  * This is where all instances of engine core interaction.
  */
 public class EngineCore {
     private static EngineController engineController;
+    private static DynamicEngineInventoryRenderer dynamicEngineInventoryRenderer;
 
     public static EngineController getEngineController() {
         return engineController;
@@ -16,4 +18,13 @@ public class EngineCore {
     public static void setEngineController(EngineController engineController) {
         EngineCore.engineController = engineController;
     }
+
+    public static DynamicEngineInventoryRenderer getDynamicEngineInventoryRenderer() {
+        return dynamicEngineInventoryRenderer;
+    }
+
+    public static void setDynamicEngineInventoryRenderer(DynamicEngineInventoryRenderer engineController) {
+        EngineCore.dynamicEngineInventoryRenderer = engineController;
+    }
+
 }
