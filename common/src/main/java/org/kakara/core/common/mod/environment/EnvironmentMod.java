7fd06c6942d6a8eec6f51ab725d41a76e409012f
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-12-12 11:27:10 -0500

    Added helper methods to EnvironmentMod and GameMod

--- common/src/main/java/org/kakara/core/common/mod/environment/EnvironmentMod.java
+++ common/src/main/java/org/kakara/core/common/mod/environment/EnvironmentMod.java
@@ -1,14 +1,11 @@
 package org.kakara.core.common.mod.environment;
 
-import org.kakara.core.common.ControllerKey;
-import org.kakara.core.common.GameInstance;
+import org.kakara.core.common.EnvironmentInstance;
 import org.kakara.core.common.Kakara;
 import org.kakara.core.common.mod.AbstractMod;
-import org.kakara.core.common.mod.Mod;
-import org.kakara.core.common.mod.ModRules;
-import org.kakara.core.common.mod.ModType;
-import org.kakara.core.common.resources.ResourceType;
-import org.slf4j.Logger;
 
 public class EnvironmentMod extends AbstractMod {
+    public EnvironmentInstance getEnvironment() {
+        return Kakara.getEnvironmentInstance();
+    }
 }
