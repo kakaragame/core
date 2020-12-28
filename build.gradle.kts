00fd9811538e415e77d400335d26cc7307c327e3
7we <peterhenryd@gmail.com>
2020-12-27 20:21:00 -0500

    Added aggregatedJavadocs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,22 @@ subprojects {
         maven("https://repo.maven.apache.org/maven2")
         maven("https://repo.kingtux.me/storages/maven/kakara")
     }
+
+}
+tasks.register<Javadoc>("aggregatedJavadocs") {
+    setDestinationDir(file("$buildDir/docs/javadoc"))
+    title = "$project.name $version API"
+    options.withGroovyBuilder {
+        "author"(true)
+        "addStringOption"("Xdoclint:none", "-quiet")
+        "addStringOption"("sourcepath", "")
+    }
+    subprojects.forEach { proj ->
+        proj.tasks.filterIsInstance<Javadoc>().forEach {
+            source += it.source
+            classpath += it.classpath
+            excludes += it.excludes
+            includes += it.includes
+        }
+    }
 }
