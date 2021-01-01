b4e6f85fa8bd3f15c041ac5be13543353389ff10
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-01 11:20:44 -0500

    ToolType and more javadocs

---
+++ common/src/main/java/org/kakara/core/common/game/ToolType.java
@@ -0,0 +1,10 @@
+package org.kakara.core.common.game;
+
+/**
+ * A ToolType is a type of tool.
+ * Currently it has not methods. This is more of a way to visualize the system.
+ */
+public interface ToolType {
+
+
+}
