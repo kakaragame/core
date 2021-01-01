c29f4c2ed69c193f1bdafd94a0493a9852c3253d
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-01 09:37:45 -0500

    Added Javadocs to ItemStack and ServerItemStack

--- server/src/main/java/org/kakara/core/server/game/ServerItemStack.java
+++ server/src/main/java/org/kakara/core/server/game/ServerItemStack.java
@@ -1,5 +1,7 @@
 package org.kakara.core.server.game;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.kakara.core.common.Serverable;
 import org.kakara.core.common.exceptions.NoServerVersionAvailableException;
 import org.kakara.core.common.game.ItemStack;
@@ -7,12 +9,31 @@ import org.kakara.core.common.game.ItemStack;
 import java.util.List;
 
 public interface ServerItemStack extends ItemStack {
+    /**
+     * Sets the number of items in the stack
+     *
+     * @param count
+     */
     void setCount(int count);
 
-    void setName(String name);
+    /**
+     * sets the name
+     *
+     * @param name the new name
+     */
+    void setName(@NotNull String name);
 
-    void setLore(List<String> lore);
+    /**
+     * Sets the lore
+     *
+     * @param lore the item lore. Can be null
+     */
+    void setLore(@Nullable List<String> lore);
 
+    /**
+     * Clones the ItemStack exactly how it is now.
+     * @return the cloned ItemStack
+     */
     ItemStack clone();
 
     @Override
