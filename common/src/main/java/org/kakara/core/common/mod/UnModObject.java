e53bb5f5919113aa2e6fde2d388efb1c831e493f
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-01 11:40:55 -0500

    Javadocs

--- common/src/main/java/org/kakara/core/common/mod/UnModObject.java
+++ common/src/main/java/org/kakara/core/common/mod/UnModObject.java
@@ -1,5 +1,12 @@
 package org.kakara.core.common.mod;
 
+/**
+ * This is one of those classes that you kinda just ignore.
+ * <p>
+ * It is used for internal uses.
+ *
+ * @since 1.0
+ */
 public class UnModObject {
     private final ModRules modRules;
     private final ClassLoader modClassLoader;
