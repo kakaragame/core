ba831efc19d4f0363d0fe40843b13a234e091718
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-02-26 19:58:52 -0500

    Finished Meter API

--- common/src/main/java/org/kakara/core/common/player/meter/PlayerMeterController.java
+++ common/src/main/java/org/kakara/core/common/player/meter/PlayerMeterController.java
@@ -1,10 +1,15 @@
 package org.kakara.core.common.player.meter;
 
+/**
+ * The class that controls the meters and levels in a player.
+ *
+ */
 public interface PlayerMeterController {
     PlayerMeter getPlayerMeter(Meter meter);
 
     /**
      * Null is never returned. It will create the needed values if
+     *
      * @param meters
      * @return
      */
