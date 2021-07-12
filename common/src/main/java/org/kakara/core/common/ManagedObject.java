18d69d56815df36a4efac3f6f9962be4dc26ecbf
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2021-07-11 18:53:44 -0700

    Allow ManagedObjects to have a percent attached to them.

--- common/src/main/java/org/kakara/core/common/ManagedObject.java
+++ common/src/main/java/org/kakara/core/common/ManagedObject.java
@@ -1,5 +1,22 @@
 package org.kakara.core.common;
 
+/**
+ * An objected that has a status and can be loaded/unloaded.
+ */
 public interface ManagedObject {
+    /**
+     * Get the status of the ManagedObject.
+     *
+     * @return The status of the ManagedObject.
+     */
     Status getStatus();
+
+    /**
+     * Get the percent for loading or unloading.
+     *
+     * <p>This is mainly used for graphical indications.</p>
+     *
+     * @return The percent as an int between 0-100.
+     */
+    int getPercent();
 }
